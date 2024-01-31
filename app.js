591e3f083e56c0ecf70ea595178bcf8584786ca7
arden <ardenbyui.edu>
2024-01-31 16:40:54 -0700

    team4andweek4peronsal v1

--- app.js
+++ app.js
@@ -1,3 +1,29 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongodb = require('./db/connect');
+
+const port = process.env.PORT || 8080;
+const app = express();
+
+app
+  .use(bodyParser.json())
+  .use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+  })
+  .use('/', require('./routes'));
+
+mongodb.initDb((err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(port);
+    console.log(`Connected to DB and listening on ${port}`);
+  }
+});
+
+
+
 // const {MongoClient} = require('mongodb');
 
 // async function main(){
@@ -50,28 +76,29 @@
 // });
 
 // *****************Above is the old code ****************
-// new code
+// MORE OLD CODE
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongodb = require('./db/connect');
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const mongodb = require('./db/connect');
 
-const port = process.env.PORT || 8080;
-const app = express();
+// const port = process.env.PORT || 8080;
+// const app = express();
 
-app
-  .use(bodyParser.json())
-  .use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    next();
-  })
-  .use('/', require('./routes'));
+// app
+//   .use(bodyParser.json())
+//   .use((req, res, next) => {
+//     res.setHeader('Access-Control-Allow-Origin', '*');
+//     next();
+//   })
+//   .use('/', require('./routes'));
 
-mongodb.initDb((err, mongodb) => {
-  if (err) {
-    console.log(err);
-  } else {
-    app.listen(port);
-    console.log(`Connected to DB and listening on ${port}`);
-  }
-});
+// mongodb.initDb((err, mongodb) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     app.listen(port);
+//     console.log(`Connected to DB and listening on ${port}`);
+//   }
+// });
+
